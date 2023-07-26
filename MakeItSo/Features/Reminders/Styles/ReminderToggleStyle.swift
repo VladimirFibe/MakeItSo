2b81a78cb4543b04359085f53c6e0e7607274f7f
Vladimir Fibe <motiw@icloud.com>
2023-07-27 01:16:26 +0600

    gitignore

---
+++ MakeItSo/Features/Reminders/Styles/ReminderToggleStyle.swift
@@ -0,0 +1,18 @@
+//
+//  ReminderToggleStyle.swift
+//  MakeItSo
+//
+//  Created by MacService on 7/26/23.
+//
+
+import SwiftUI
+
+struct ReminderToggleStyle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReminderToggleStyle()
+}
---
+++ MakeItSo/Features/Reminders/Views/RemindersListRowView.swift
@@ -0,0 +1,18 @@
+//
+//  RemindersListRowView.swift
+//  MakeItSo
+//
+//  Created by MacService on 7/26/23.
+//
+
+import SwiftUI
+
+struct RemindersListRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RemindersListRowView()
+}
